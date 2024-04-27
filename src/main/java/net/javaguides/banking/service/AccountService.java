c081d772f1c11d08afe4d2ea91a4f83b45d20398
Srikanta kumar Rout <srout9333@gmail.com>
2024-04-28 00:50:28 +0530

    initial commit

---
+++ src/main/java/net/javaguides/banking/service/AccountService.java
@@ -0,0 +1,26 @@
+package net.javaguides.banking.service;
+
+
+
+
+
+import java.util.List;
+
+import net.javaguides.banking.dto.AccountDto;
+
+public interface AccountService {
+	
+	AccountDto createAccount(AccountDto accountDto);
+	
+	AccountDto getAccountById(Long id);
+	
+	AccountDto deposite(Long id,double amount);
+	
+	AccountDto withdraw(Long id,double amount);
+	
+	List<AccountDto> getAllAccounts();
+	
+	void deleteAccount(Long id);
+	
+
+}
